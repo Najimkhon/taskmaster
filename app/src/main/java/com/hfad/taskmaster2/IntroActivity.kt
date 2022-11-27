bd6f3eea39d464e399b13a4cf0b2a8035c414874
Mahkamov Najimkhon <qwerty1995.290@gmail.com>
2022-11-27 12:56:26 +0500

    IntroActivity is added

---
+++ app/src/main/java/com/hfad/taskmaster2/IntroActivity.kt
@@ -0,0 +1,26 @@
+package com.hfad.taskmaster2
+
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import com.hfad.taskmaster2.databinding.ActivityIntroBinding
+import com.hfad.taskmaster2.databinding.ActivitySplashBinding
+
+
+class IntroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        val typFace: Typeface = Typeface.createFromAsset(assets, "carbon bl.ttf")
+        binding.tvAppNameIntro.typeface = typFace
+    }
+}
--- app/src/main/java/com/hfad/taskmaster2/SplashActivity.kt
+++ app/src/main/java/com/hfad/taskmaster2/SplashActivity.kt
@@ -1,8 +1,10 @@
 package com.hfad.taskmaster2
 
+import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.WindowManager.LayoutParams.*
 import com.hfad.taskmaster2.databinding.ActivitySplashBinding
 
@@ -21,6 +23,11 @@ class SplashActivity : AppCompatActivity() {
         )
         val typFace: Typeface = Typeface.createFromAsset(assets, "carbon bl.ttf")
         binding.tvAppName.typeface = typFace
+
+        Handler().postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+        }, 2500)
 
     }
 }
